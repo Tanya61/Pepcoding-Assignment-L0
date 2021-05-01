8960910fd5ca996570892a8a133db9240242fec0
Tanya Singhal <55254399+Tanya61@users.noreply.github.com>
2021-05-01 22:08:33 +0530

    Create program_22.java
    Write a program to find largest among three numbers using conditional operator.

---
+++ program_22.java
@@ -0,0 +1,24 @@
+// Write a program to find largest among three numbers using conditional operator. 
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+        if(a>b && a>c){
+            System.out.println(a);
+        }
+        else if(b>c){
+            System.out.println(b);
+        }
+        else{
+            System.out.println(c);
+        }
+        
+    }
+}
+
+//Input 7 8 9
+//Output 9
