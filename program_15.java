2bf3817fc1cd024f5681a6766871faf8f57d2187
Tanya Singhal <55254399+Tanya61@users.noreply.github.com>
2021-05-01 21:09:21 +0530

    Create program_15.java
    Write a program to print hello world without using semicolon at the end of print statement .

---
+++ program_15.java
@@ -0,0 +1,11 @@
+//Write a program to print hello world without using semicolon at the end of print statement .
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        if (System.out.printf("Hello World") == null) {
+        }
+    }
+}
+
+//Output: Hello World
