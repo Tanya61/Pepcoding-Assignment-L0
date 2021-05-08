80bbcf102973946da6046eb4b27bf3ccdc7f5b8a
Tanya Singhal <55254399+Tanya61@users.noreply.github.com>
2021-05-08 12:28:19 +0530

    Create program__45.java
    Write a program to convert hexadecimal number to binary number.

---
+++ program__45.java
@@ -0,0 +1,14 @@
+// Write a program to convert hexadecimal number to binary number. 
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner scn= new Scanner(System.in);
+        String hex = scn.nextLine();
+        int dec=Integer.parseInt(hex,16);
+        String bin = Integer.toBinaryString(dec);
+        System.out.println(bin);
+    }
+}
+
+//Input : 1AC5
+//Output : 1101011000101
